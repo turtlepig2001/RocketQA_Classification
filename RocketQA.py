b8bb33d7faefe213f81a0b6557a2a1bf80837505
承念 <1791708451@qq.com>
2023-10-05 21:53:17 +0800

    finish data utils

--- RocketQA.py
+++ RocketQA.py
@@ -1,7 +1,7 @@
 '''
 Date: 2023-09-23 22:52:19
 LastEditors: turtlepig
-LastEditTime: 2023-09-23 23:03:07
+LastEditTime: 2023-09-27 21:50:02
 Description:  RocketQA Classification
 '''
 
@@ -17,7 +17,10 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 
 import transformers
 from transformers import AutoTokenizer,AutoModelForSequenceClassification
+
+
 
