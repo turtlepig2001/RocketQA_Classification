6972f0618b58a6c0f3d7e2ef3dfb06e31016ea40
turtlepig2001 <turtlepig200101@163.com>
2023-11-20 00:07:35 +0800

    init basemodel

--- model.py
+++ model.py
@@ -8,9 +8,26 @@ import abc
 import sys
 
 import numpy as np
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class SemanticIndexBase(nn.Module):
     '''
     '''
+    def __init__(self, pretrained_model, dropout = None, output_emb_size = None) :
+        super().__init__()
+        self.ptm = pretrained_model
+        self.dropout = nn.Dropout(dropout if dropout is None else 0.1)
+
+        # if output_emb_size is not None, then add Linear layer to reduce embedding_size,
+        # we recommend set output_emb_size = 256 considering the trade-off beteween
+        # recall performance and efficiency
+
+        self.output_emb_size = output_emb_size
+        if output_emb_size > 0:
+            # weight_attr = nn.parameter.Parameter(nn.init.trunc_normal_(torch.empty(768, output_emb_size), std = 0.02))
+            self.emb_reduce_linear = nn.Linear(768, output_emb_size)
+
+
+
